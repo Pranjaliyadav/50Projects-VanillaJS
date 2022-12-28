0bb061ca688527271d1eda3e61f266c2130f6509
Pranjaliyadav <yadavpranjali1223@gmail.com>
2022-12-28 23:16:46 +0530

    d22

--- D22_DrawingApp/script.js
+++ D22_DrawingApp/script.js
@@ -4,7 +4,9 @@ const decreaseBtn = document.getElementById('decrease');
 const sizeEL = document.getElementById('size');
 const colorEl = document.getElementById('color');
 const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext("2d");
+//this ctx is canvas screen 2d
 
 let size = 10;
 let isPressed = false;
@@ -14,7 +16,7 @@ let y;
 
 canvas.addEventListener("mousedown", (e) => {
   isPressed = true;
-  x = e.offsetX;
+  x = e.offsetX; //exact point where we press
   y = e.offsetY;
 
 //   console.log(isPressed, x, y);
@@ -31,15 +33,16 @@ if(isPressed){
     const x2 = e.offsetX
     const y2 = e.offsetY
 
-    drawCircle(x2,y2)
-    drawLine(x,y,x2,y2)
-    x = x2
+    drawCircle(x2,y2) //we drawing circle on each impact but if we movve fast there will be spaces
+    drawLine(x,y,x2,y2) // so using line for those spaces
+    x = x2 //and then updating the starting or every line to where we left off so its continues.
     y=y2
 }
 });
 
 function drawCircle(x, y) {
-  ctx.beginPath();
+    //this way we draw circle-refer canvas documentation
+  ctx.beginPath(); 
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fillStyle = color;
   ctx.fill();
@@ -48,10 +51,10 @@ function drawCircle(x, y) {
 function drawLine(x1, y1, x2, y2) {
   //move to, line to
   ctx.beginPath();
-  ctx.moveTo(x1, y1);
+  ctx.moveTo(x1, y1); 
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size*2;
+  ctx.lineWidth = size*2; // so the lines be same size as circles
   ctx.stroke();
 }
 
@@ -90,4 +93,4 @@ decreaseBtn.addEventListener('click',()=>{
     updateSizeOnScreen()
 })
 
-clearEl.addEventListener('click',()=> ctx.clearRect(0,0,canvas.width, canvas.height))
+clearEl.addEventListener('click',()=> ctx.clearRect(0,0,canvas.width, canvas.height)) //now we just clear it off
